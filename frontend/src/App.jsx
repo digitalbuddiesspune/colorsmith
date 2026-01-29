60ed90206b8a34dfcecca73c596506cf58c68296
Rishikesh5577 <rishiwakchaure2003@gmail.com>
2026-01-29 15:47:25 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center p-4">
+      <h1 className="text-2xl font-medium text-slate-300">
+        Basic setup is complete
+      </h1>
+    </div>
+  )
+}
+
+export default App
